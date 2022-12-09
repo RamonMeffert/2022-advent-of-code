80180a0c6d1c52de0393a439eb45589ec848b213
Ramon Meffert <ramon.meffert@gmail.com>
2022-12-09 21:03:30 +0100

    prettify ts

--- 9/9.ts
+++ 9/9.ts
@@ -4,107 +4,130 @@ import * as fs2 from "fs";
 await main();
 
 async function main() {
-    const input = await fs.readFile("input", "utf-8");
+  const input = await fs.readFile("input", "utf-8");
 
-    // All knots start at the same location
-    const numKnots = parseInt(process.argv[2]) || 2;
+  // All knots start at the same location
+  const numKnots = parseInt(process.argv[2]) || 2;
 
-    // Tried Array.fill here first, but that reuses the same object. so all
-    // items in the array point to the same object 
-    let rope: Location[] = [];
-    for (let i = 0; i < numKnots; i++) {
-        rope[i] = { x: 0, y: 0};
+  // Tried Array.fill here first, but that reuses the same object. so all
+  // items in the array point to the same object
+  let rope: Location[] = [];
+  for (let i = 0; i < numKnots; i++) {
+    rope[i] = { x: 0, y: 0 };
+  }
+
+  // Keep track of where the tail has been using a set
+  let tailVisited: Set<string> = new Set();
+
+  for (const move of input.split("\n")) {
+    const [dir, num] = move.split(" ");
+    for (let i = 0; i < parseInt(num); i++) {
+      rope[0] = moveHead(rope[0], dir);
+      for (let j = 1; j < numKnots; j++) {
+        rope[j] = moveTail(rope[j], rope[j - 1]);
+      }
+      tailVisited.add(JSON.stringify(rope[numKnots - 1]));
     }
+  }
 
-    // Keep track of where the tail has been using a set
-    let tailVisited: Set<string> = new Set();
+  // Save a map of all tail visits
+  saveTailVisitsMapToFile(tailVisited);
 
-    for (const move of input.split("\n")) {
-        const [dir, num] = move.split(" ");
-        for (let i = 0; i < parseInt(num); i++) {
-            rope[0] = moveHead(rope[0], dir);
-            for (let j = 1; j < numKnots; j++) {
-                rope[j] = moveTail(rope[j], rope[j - 1]);
-            }
-            tailVisited.add(JSON.stringify(rope[numKnots - 1]));
-        }
-    }
-
-    // Save a map of all tail visits
-    saveTailVisitsMapToFile(tailVisited);
-
-    // Print result
-    console.log(tailVisited.size);
+  // Print result
+  console.log(tailVisited.size);
 }
 
 function moveHead(head: Location, direction: string): Location {
-    switch (direction) {
-        case "L": head.x--; break;
-        case "R": head.x++; break;
-        case "U": head.y--; break;
-        case "D": head.y++; break;
-    }
+  switch (direction) {
+    case "L":
+      head.x--;
+      break;
+    case "R":
+      head.x++;
+      break;
+    case "U":
+      head.y--;
+      break;
+    case "D":
+      head.y++;
+      break;
+  }
 
-    return head;
+  return head;
 }
 
 function moveTail(tail: Location, head: Location): Location {
-    const d = distance(tail, head);
-    if (d <= Math.hypot(1, 1)) {
-        return tail;
-    } else {
-        const dx = head.x - tail.x;
-        const dy = head.y - tail.y;
-        tail.x += Math.sign(dx);
-        tail.y += Math.sign(dy);
-        return tail;
-    }
+  const d = distance(tail, head);
+  if (d <= Math.hypot(1, 1)) {
+    return tail;
+  } else {
+    const dx = head.x - tail.x;
+    const dy = head.y - tail.y;
+    tail.x += Math.sign(dx);
+    tail.y += Math.sign(dy);
+    return tail;
+  }
 }
 
 function distance(p1: Location, p2: Location) {
-    return Math.hypot(p1.x - p2.x, p1.y - p2.y);
+  return Math.hypot(p1.x - p2.x, p1.y - p2.y);
 }
 
 function saveTailVisitsMapToFile(visits: Set<string>, headLocation?: Location) {
-    let minX = 0, minY = 0, maxX = 0, maxY = 0;
-    for (const visit of visits) {
-        const loc = JSON.parse(visit) as Location;
-        minX = loc.x < minX ? loc.x : minX;
-        minY = loc.y < minY ? loc.y : minY;
-        maxX = loc.x > maxX ? loc.x : maxX;
-        maxY = loc.y > maxY ? loc.y : maxY;
+  let minX = 0,
+    minY = 0,
+    maxX = 0,
+    maxY = 0;
+  for (const visit of visits) {
+    const loc = JSON.parse(visit) as Location;
+    minX = loc.x < minX ? loc.x : minX;
+    minY = loc.y < minY ? loc.y : minY;
+    maxX = loc.x > maxX ? loc.x : maxX;
+    maxY = loc.y > maxY ? loc.y : maxY;
+  }
+  minX = Math.abs(minX);
+  minY = Math.abs(minY);
+
+  function setLocation(
+    a: Array<Array<string>>,
+    x: number,
+    y: number,
+    v: string
+  ) {
+    if (a[y] === undefined) {
+      a[y] = [];
     }
-    minX = Math.abs(minX);
-    minY = Math.abs(minY);
+    a[y][x] = a[y][x] ? a[y][x] : v;
+  }
 
-    function setLocation(a: Array<Array<string>>, x: number, y: number, v: string) {
-        if (a[y] === undefined) {
-            a[y] = [];
-        }
-        a[y][x] = a[y][x] ? a[y][x] : v;
-    }
+  const visitLocations = new Array<Array<string>>();
 
-    const visitLocations = new Array<Array<string>>();
+  setLocation(visitLocations, minX, minY, "s");
 
-    setLocation(visitLocations, minX, minY, "s");
+  for (const visit of visits) {
+    const loc = JSON.parse(visit) as Location;
+    setLocation(visitLocations, minX + loc.x, minY + loc.y, "#");
+  }
 
-    for (const visit of visits) {
-        const loc = JSON.parse(visit) as Location;
-        setLocation(visitLocations, minX + loc.x, minY + loc.y, "#");
-    }
+  if (headLocation) {
+    setLocation(
+      visitLocations,
+      minX + headLocation.x,
+      minY + headLocation.y,
+      "H"
+    );
+  }
 
-    if (headLocation) {
-        setLocation(visitLocations, minX + headLocation.x, minY + headLocation.y, "H")
-    }
-
-    const output = fs2.createWriteStream("output");
-    for (let row of visitLocations) {
-        row[minX + maxX] = row[minX + maxX] ? row[minX + maxX] : ".";
-        output.write(Array.from(row, x => x === undefined ? "." : x).join("") + "\n");
-    }
+  const output = fs2.createWriteStream("output");
+  for (let row of visitLocations) {
+    row[minX + maxX] = row[minX + maxX] ? row[minX + maxX] : ".";
+    output.write(
+      Array.from(row, (x) => (x === undefined ? "." : x)).join("") + "\n"
+    );
+  }
 }
 
 interface Location {
-    x: number,
-    y: number
+  x: number;
+  y: number;
 }
